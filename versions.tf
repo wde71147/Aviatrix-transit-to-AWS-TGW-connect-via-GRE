5aa933042afd4a8277d04ca8d7a7a078bb3462be
wde71147 <97315628+wde71147@users.noreply.github.com>
2022-06-27 23:00:17 -0600

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    aviatrix = {
+      source = "aviatrixsystems/aviatrix"
+    }
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  required_version = ">= 1.0"
+}
